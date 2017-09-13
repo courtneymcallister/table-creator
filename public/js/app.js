fd17330b8ae650fce21395aca559f406d6d35b77
courtneymcallister <coa.mcallister@gmail.com>
2017-09-13 13:18:31 -0400

    Implemented jquery. Each td in the table created is an input field. Added a clear button.

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,36 @@
-function updateTable(){
-  var table = document.getElementById('table');
+$(document).ready(function(){
+  const table = document.getElementById('table');
 
-  //get user inputs
-  var rows = document.getElementById('rows').value;
-  var cols = document.getElementById('cols').value;
-  var caption = document.getElementById('caption').value;
 
-  //add rows and columns
-  for(var i = 0; i < rows; i++){
-    var newRow = table.insertRow(i);
-    for(var j = 0; j < cols; j++){
-      var newCell = newRow.insertCell(j);
-      newCell.appendChild(document.createTextNode(`cell ${i} - ${j}`))
+  $(".createTable").click(function(){
+    table.innerHTML = "";
+    //get user inputs
+    var rows = $('#rows').val();
+    var cols = $("#cols").val();
+    var caption = $("#caption").val();
+
+    //add rows and columns
+    for(var i = 0; i < rows; i++){
+      var newRow = table.insertRow(i);
+      for(var j = 0; j < cols; j++){
+        var newCell = newRow.insertCell(j);
+        newCell.appendChild(document.createElement("input"))
+      }
     }
-  }
 
-  //add caption (optional)
-  var newCaption = table.createCaption();
-  newCaption.innerHTML = `${caption}`;
+    //add caption (optional)
+    var newCaption = table.createCaption();
+    newCaption.innerHTML = `${caption}`;
+  })
 
-}
+  $(".clearTable").click(function(){
+    table.innerHTML = '';
+    var field = document.getElementsByClassName("showHtmlField")
+    field.innerHTML = '';
+  })
+
+  $(".showHtml").click(function(){
+    $(".showHtmlField").text($("table").html());
+  })
+
+})
